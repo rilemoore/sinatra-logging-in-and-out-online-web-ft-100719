9ac914637e1f9eded17b5b132bf6d0e524671368
Riley Moore <rilemoore@gmail.com>
2019-11-30 22:08:32 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,6 @@
 class Helpers
   def self.current_user(session)
-    @user = User.where(user_id: session[:user_id]) 
+    @user = User.find(session[:user_id])
   end
   
   def self.is_logged_in?(session)
