07dda5f270601a74e83df0dae973e25b12c5bedf
Riley Moore <rilemoore@gmail.com>
2019-11-30 20:55:13 +0000

    Automatically backed up by Learn

--- db/migrate/20191130205409_create_users.rb
+++ db/migrate/20191130205409_create_users.rb
@@ -1,5 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    
+    def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :content
+
+    end
+  end
   end
 end
